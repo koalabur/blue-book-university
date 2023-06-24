a0ad80f6670e4287f44f83098a692716f23c0a2a
Cameron Roberts <cameronrdesign@gmail.com>
2023-06-24 10:57:48 -0700

    fix: update next.config with envs

--- next.config.js
+++ next.config.js
@@ -28,6 +28,8 @@ const nextConfig = {
   env: {
     MONGODB_URI: process.env.MONGODB_URI,
     JWT_SECRET: process.env.JWT_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
   },
 };
 
